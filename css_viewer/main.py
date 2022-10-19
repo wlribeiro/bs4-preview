888b466d844a110be61147a531bd5059df2a3c90
William <william.ribeiro@jusbrasil.com.br>
2022-10-19 14:00:13 -0300

    add mulit selector

--- css_viewer/main.py
+++ css_viewer/main.py
@@ -18,28 +18,29 @@ app = FastAPI()
 class Item(BaseModel):
     html: str
     css_selector: str
-
+    multi_selector: bool
 
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
+
 
 @app.get("/", response_class=HTMLResponse)
 def index(request: Request) -> Request:
     context = {"request": request}
     return templates.TemplateResponse("index.html", context)
 
+
 @app.post("/run")
 async def run_css_selector(data: Item):
-    result = extract(data.html, data.css_selector)
+    result = extract(data.html, data.css_selector, data.multi_selector)
     return {"response": str(result)}
 
 
-def request_and_extract(url):
-    response = requests.get(url)
-
-
-def extract(html, selector):
+def extract(html: str, selector: str, multi_selector: bool = False):
     soup = BeautifulSoup(html, "html.parser")
-    result = soup.select_one(selector)
-    return result
+    result = soup.select(selector)
+    if result:
+        return result if multi_selector else result[0]
+    
+    return None
